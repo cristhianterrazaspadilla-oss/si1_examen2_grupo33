c831fa1fb60ce0eb5a5859465b367b81f529d12d
Daniel_Snake <snakeapaza@gmail.com>
2026-06-09 23:24:12 -0400

    Agregar configuración Docker para Render

---
+++ cupcore/docker/start-container.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+PORT="${PORT:-10000}"
+
+sed -ri "s/^Listen [0-9]+$/Listen ${PORT}/" /etc/apache2/ports.conf
+sed -ri "s/<VirtualHost \*:[0-9]+>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+
+mkdir -p \
+    bootstrap/cache \
+    storage/app/public \
+    storage/framework/cache/data \
+    storage/framework/sessions \
+    storage/framework/views \
+    storage/logs
+
+chown -R www-data:www-data bootstrap/cache storage
+
+php artisan config:clear
+php artisan config:cache
+php artisan storage:link --force || true
+
+exec "$@"
