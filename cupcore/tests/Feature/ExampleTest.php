c0f15fcec971e17eccfe7a92bb12e7273644c23f
Daniel_Snake <snakeapaza@gmail.com>
2026-06-10 23:39:59 -0400

    Corregir URLs HTTPS detrás de Render

--- cupcore/tests/Feature/ExampleTest.php
+++ cupcore/tests/Feature/ExampleTest.php
@@ -1,7 +1,26 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 test('the application returns a successful response', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
+});
+
+test('trusted proxy headers generate secure urls', function () {
+    Route::get('/test-secure-url', fn () => route('gestion-academica-cup.reportes.interpretar-comando'));
+
+    $response = $this
+        ->withServerVariables(['REMOTE_ADDR' => '10.0.0.1'])
+        ->withHeaders([
+            'X-Forwarded-Host' => 'si1-examen2-grupo33.onrender.com',
+            'X-Forwarded-Port' => '443',
+            'X-Forwarded-Proto' => 'https',
+        ])
+        ->get('/test-secure-url');
+
+    $response
+        ->assertOk()
+        ->assertSeeText('https://si1-examen2-grupo33.onrender.com/gestion-academica-cup/reportes/interpretar-comando');
 });
